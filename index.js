e1a7697ead67e127f32cded41bdbadef7bb5ddfc
soniasankpal <soniasankpal@gmail.com>
2021-01-16 21:16:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,123 @@
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style';
+import {Draw, Modify, Snap} from 'ol/interaction';
+import {OSM, Vector as VectorSource} from 'ol/source';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
+
+var raster = new TileLayer({
+  source: new OSM(),
+});
+
+var source = new VectorSource();
+var vector = new VectorLayer({
+  source: source,
+  style: new Style({
+    fill: new Fill({
+      color: 'rgba(255, 255, 255, 0.2)',
+    }),
+    stroke: new Stroke({
+      color: '#ffcc33',
+      width: 2,
+    }),
+    image: new CircleStyle({
+      radius: 7,
+      fill: new Fill({
+        color: '#ffcc33',
+      }),
+    }),
+  }),
+});
+
+var map = new Map({
+  layers: [raster, vector],
+  target: 'map',
+  view: new View({
+    center: [-11000000, 4600000],
+    zoom: 4,
+  }),
+  
+});
+
+var modify = new Modify({source: source});
+map.addInteraction(modify);
+
+var draw, snap; // global so we can remove them later
+var typeSelect = document.getElementById('type');
+
+function addInteractions() {
+  draw = new Draw({
+    source: source,
+    type: typeSelect.value,
+  });
+  
+  map.addInteraction(draw);
+  draw.setActive(true);
+  snap = new Snap({source: source,edge:true,});
+  snap.setActive(true);
+  
+  map.addInteraction(snap);
+  
+  draw.on('drawstart',function(e){
+    //console.log("Co Ordinates :",e.feature.getGeometry().transform('EPSG:3857', 'EPSG:4326'))
+    //console.log("Co Ordinates :",e.feature.getGeometry().getExtent())
+    var geom = e.feature.getGeometry().transform('EPSG:3857', 'EPSG:4326');
+    console.log(geom.getCoordinates())  
+  });
+}
+
+
+
+/**
+ * Handle change event.
+ */
+typeSelect.onchange = function () {
+  map.removeInteraction(draw);
+  map.removeInteraction(snap);
+  addInteractions();
+};
+
+addInteractions();
+console.log(snap);
+
+document.getElementById('export-png').addEventListener('click', function () {
+  map.once('rendercomplete', function () {
+    var mapCanvas = document.createElement('canvas');
+    var size = map.getSize();
+    mapCanvas.width = size[0];
+    mapCanvas.height = size[1];
+    var mapContext = mapCanvas.getContext('2d');
+    Array.prototype.forEach.call(
+      document.querySelectorAll('.ol-layer canvas'),
+      function (canvas) {
+        if (canvas.width > 0) {
+          var opacity = canvas.parentNode.style.opacity;
+          mapContext.globalAlpha = opacity === '' ? 1 : Number(opacity);
+          var transform = canvas.style.transform;
+          // Get the transform parameters from the style's transform matrix
+          var matrix = transform
+            .match(/^matrix\(([^\(]*)\)$/)[1]
+            .split(',')
+            .map(Number);
+          // Apply the transform to the export map context
+          CanvasRenderingContext2D.prototype.setTransform.apply(
+            mapContext,
+            matrix
+          );
+          mapContext.drawImage(canvas, 0, 0);
+        }
+      }
+    );
+    if (navigator.msSaveBlob) {
+      // link download attribuute does not work on MS browsers
+      navigator.msSaveBlob(mapCanvas.msToBlob(), 'map.png');
+    } else {
+      var link = document.getElementById('image-download');
+      link.href = mapCanvas.toDataURL();
+      link.click();
+    }
+  });
+  map.renderSync();
+});
+
